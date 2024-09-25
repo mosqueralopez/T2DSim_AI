ecc58c8ef85919da3c3e0b9e730094232f85d8af
vlt-ro <valentina.roquemen@udea.edu.co>
2024-09-24 21:02:07 -0700

    add: README on NeuralOGTT

--- README.md
+++ README.md
@@ -8,6 +8,9 @@
 ## Table of Contents
 
 - [Installation](#installation)
+- [Simulation: NeuralOGTT and DT-NeuralOGTT](#simulation-neuralogtt-and-dt-neuralogtt)
+- [Creation of a Digital Twin](#creation-of-a-digital-twin)
+- [Citation](#citation)
 - [License](#license)
 
 ## Installation
@@ -15,6 +18,41 @@
 ```console
 pip install t2dsim-ai
 ```
+## Simulation: NeuralOGTT and DT-NeuralOGTT
+
+### NeuralOGTT
+
+NeuralOGTT is a novel hybrid approach to modeling glucose-insulin dynamics during the oral glucose tolerance test (OGTT).
+
+Run the following command to simulate an OGTT, with the initial fasting glucose as the only input. To modify the input, change the variable `initCGM`.
+
+```bash
+python example/runOGTT.py
+```
+
+After running the example, the simulation will appear as follows for `initCGM = 100 mg/dL`:
+![OGTTExample](example/example_neuralOGTT_fastingGlucose_100.png)
+
+### DT-NeuralOGTT
+
+WIP
+
+## Creation of a Digital Twin
+
+## Citation
+
+If you used this package in your research, please cite it:
+
+```
+@Misc{,
+    author = {Valentina Roquemen-Echeverri and Clara Mosquera-Lopez},
+    title = {T2DSim AI},
+    year = {2024--},
+    url = "https://github.com/mosqueralopez/T2DSim_AI"
+}
+```
+
+TODO: Add final work
 
 ## License
 
--- example/runOGTT.py
+++ example/runOGTT.py
@@ -1,6 +1,8 @@
 from t2dsim_ai.create_scenarios import ogtt_scenario
 from t2dsim_ai.model_NeuralOGTT import NeuralOGTT
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
 import numpy as np
 
 # Simulation
@@ -14,7 +16,7 @@ df_simulation = myOGTT.simulate(ogtt_scenario(initCGM))
 state_col = ['state_Gc','state_I']
 state_name = ['Glucose in plasma [mg/dL]','Insulin in plasma [mu/L]']
 
-fig, ax = plt.subplots(figsize =(9, 7),ncols=1, nrows=2)
+fig, ax = plt.subplots(figsize =(15, 7),ncols=1, nrows=2)
 
 SMALL_SIZE = 15
 plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
@@ -22,13 +24,17 @@ plt.rc('axes', titlesize=SMALL_SIZE)
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["text.usetex"] = False
 
-plt.suptitle('NeuralOGTT - Initial CGM:'+str(initCGM)+' mg/dL')
+legend_elements = [Line2D([0], [0], color='gray', linestyle='--',alpha=0.5, label='Glucose Dose: 75 g')]
+
+leg = fig.legend( handles = legend_elements, loc='upper center',bbox_to_anchor=(0.5, 0.9),ncol=1,fontsize="15",facecolor='white', framealpha=1, frameon=False)
+
 for i in [0,1]:
     ax[i].plot(df_simulation['time'],df_simulation[state_col[i]])
-    ax[i].set_ylabel(state_name[i])
+    ax[i].set_ylabel(state_name[i],fontsize=15)
     ax[i].axvline(15,ls='--',c='gray',alpha=0.5)
 
     ax[i].tick_params(axis='y', labelsize=15)
+    ax[i].tick_params(axis='x', labelsize=15)
 
     for side in ['right','top']:
         ax[i].spines[side].set_visible(False)
@@ -36,5 +42,9 @@ for i in [0,1]:
     for side in ['left','bottom']:
         ax[i].spines[side].set(alpha=1)
 
-ax[1].set_xlabel('Time [min]')
+ax[0].set_title('NeuralOGTT - Fasting Glucose:'+str(initCGM)+' mg/dL')
+ax[0].set_ylim(40,300)
+ax[1].set_xlabel('Time [min]',fontsize=15)
+
 plt.show()
+#plt.savefig('example_neuralOGTT_fastingGlucose_'+str(initCGM)+'.png',dpi=500, bbox_extra_artists=(leg,), bbox_inches='tight')
